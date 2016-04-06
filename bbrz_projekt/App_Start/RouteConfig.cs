f5cf233a4ea7cd6bc2febed61123cfe95503210a
zeitlwiga <zeiti16@gmail.com>
2016-04-06 17:55:37 +0200

    llökkop

--- bbrz_projekt/App_Start/RouteConfig.cs
+++ bbrz_projekt/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace igdb
+namespace bbrz_projekt
 {
     public class RouteConfig
     {
@@ -16,8 +16,9 @@ namespace igdb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
 }
+
--- bbrz_projekt/Properties/AssemblyInfo.cs
+++ bbrz_projekt/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die
 // Assemblyinformationen zu ändern.
-[assembly: AssemblyTitle("igdb")]
+[assembly: AssemblyTitle("bbrz_projekt")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("igdb")]
+[assembly: AssemblyProduct("bbrz_projekt")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird.
-[assembly: Guid("023b3082-5526-4cef-9825-880e74a7f7d9")]
+[assembly: Guid("79553936-086d-4105-9e15-66ff9db83332")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
